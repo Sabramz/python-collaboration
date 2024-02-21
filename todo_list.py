273c7f4c97a258058eda10fe7eaf0c64bc39e86c
Scott A <89099102+Sabramz@users.noreply.github.com>
2024-02-21 10:17:28 -0500

    main function for todo list function

--- todo_list.py
+++ todo_list.py
@@ -21,3 +21,24 @@ def view_tasks():
 
 def mark_completed(name):
 	tasks[name] = True
+
+def main():
+	print("Welcome to the to-do list")
+	while True:
+		inp = input("Enter 1 to add a task, 2 to delete a task, 3 to view tasks, and 4 to mark a task as completed, e to exit: ")
+		if inp == "1":
+			inp = input("Enter the name of the task: ")
+			add_task(inp)
+		elif inp == "2":
+			inp = input("Enter the name of the task: ")
+			delete_task(inp)
+		elif inp == "3":
+			view_tasks()
+		elif inp == "4":
+			inp = input("Enter the name of the task: ")
+			mark_completed(inp)
+		elif inp == "e":
+			print("Terminating...")
+			return
+
+main()
